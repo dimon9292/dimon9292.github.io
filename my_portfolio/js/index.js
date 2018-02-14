fe6a22ce389bc7479ed92e0e8c50d8cc2de9f2a8
dimon9292 <dimon_9292@mail.ru>
2018-02-14 18:51:01 +0300

    Сайт-портфолио
    Мой сайт-портфолио

---
+++ my_portfolio/js/index.js
@@ -0,0 +1,24 @@
+// // JavaScript File
+
+"use strict"
+
+$(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "../php/mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
